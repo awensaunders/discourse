091b46a6c2c057d98e9768bb42e513494cf58d9b
Loïc Guitaut <loic@discourse.org>
2025-09-26 17:53:03 +0200

    DEV: Move more data into the server session

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -904,12 +904,14 @@ RSpec.configure do |config|
 
   class TestCurrentUserProvider < Auth::DefaultCurrentUserProvider
     def log_on_user(user, session, cookies, opts = {})
-      session[:current_user_id] = user.id
+      # Ensure using the main session as `session` sometimes is a server session
+      cookies.request.session[:current_user_id] = user.id
       super
     end
 
     def log_off_user(session, cookies)
-      session[:current_user_id] = nil
+      # Ensure using the main session as `session` sometimes is a server session
+      cookies.request.session[:current_user_id] = nil
       super
     end
   end
@@ -1130,7 +1132,7 @@ def swap_2_different_characters(str)
 end
 
 def create_request_env(path: nil)
-  env = Rails.application.env_config.dup
+  env = Rails.application.env_config.dup.merge("rack.session" => ActionController::TestSession.new)
   env.merge!(Rack::MockRequest.env_for(path)) if path
   env
 end
