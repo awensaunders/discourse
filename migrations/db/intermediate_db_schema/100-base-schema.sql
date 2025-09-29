860d59c4993f398c0f361223ac4c929bfcf17755
Selase Krakani <selase@discourse.org>
2025-09-29 16:15:26 +0000

    DEV: Add support for converting and importing `topic_users`
    This adds converter(Discourse-only, for now) and importer steps for `topic_users`

--- migrations/db/intermediate_db_schema/100-base-schema.sql
+++ migrations/db/intermediate_db_schema/100-base-schema.sql
@@ -191,6 +191,23 @@ CREATE TABLE tags
     name        TEXT     NOT NULL
 );
 
+CREATE TABLE topic_users
+(
+    topic_id                 NUMERIC  NOT NULL,
+    user_id                  NUMERIC  NOT NULL,
+    cleared_pinned_at        DATETIME,
+    first_visited_at         DATETIME,
+    last_emailed_post_number INTEGER,
+    last_posted_at           DATETIME,
+    last_read_post_number    INTEGER,
+    last_visited_at          DATETIME,
+    notification_level       INTEGER,
+    notifications_changed_at DATETIME,
+    notifications_reason_id  NUMERIC,
+    total_msecs_viewed       INTEGER,
+    PRIMARY KEY (topic_id, user_id)
+);
+
 CREATE TABLE user_associated_accounts
 (
     provider_name TEXT      NOT NULL,
